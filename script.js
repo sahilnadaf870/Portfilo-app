e19c74073dd9f3d78b82c54c93d958abff443e7a
Sahil nadaf <nadafsahil113@gmail.com>
2025-09-25 06:13:59 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showSection(id) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(section => section.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
